60b89fe342e5caa9747005814619b9cd76085672
Arthur Schroder Dourado Mota <arthur.mota@dbccompany.com.br>
2023-12-27 09:58:51 -0300

    Criando exercício 04

---
+++ 01-java/Aula-01/src/Exercicio04.java
@@ -0,0 +1,13 @@
+public class Exercicio04 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c;
+
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println("Variável A: " + a + "\nVariável B: " + b);
+    }
+}
