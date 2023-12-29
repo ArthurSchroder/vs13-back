404b005c3e828291d64304e01cf3a85eefce8d72
Arthur Schroder Dourado Mota <arthur.mota@dbccompany.com.br>
2023-12-29 10:58:37 -0300

    Implementando novos métodos

--- 01-java/Aula-03/src/entities/ContaCorrente.java
+++ 01-java/Aula-03/src/entities/ContaCorrente.java
@@ -9,22 +9,44 @@ public class ContaCorrente {
 
 
     public void imprimirContaCorrente(){
-
+        System.out.println("ContaCorrente{" +
+                "cliente=" + cliente +
+                ", numeroConta='" + numeroConta + '\'' +
+                ", agencia=" + agencia +
+                ", saldo=" + saldo +
+                ", chequeEspecial=" + chequeEspecial +
+                '}');
     }
     public boolean sacar(double saque){
-
+        if (saque <= retornarSaldoComChequeEspecial()){
+            saldo = saldo - saque;
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public boolean depositar (double deposito){
-
+        if(deposito > 0) {
+            saldo += deposito;
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public double retornarSaldoComChequeEspecial(){
-
+        return saldo + chequeEspecial;
     }
 
     public boolean transeferir(ContaCorrente contaCorrente, double valor){
-
+        if (retornarSaldoComChequeEspecial() >= valor){
+            this.sacar(valor);
+            contaCorrente.depositar(valor);
+            return true;
+        } else{
+            return false;
+        }
     }
 }
 
